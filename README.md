ace6bd0d21268d90c1dfc4f23420ebeb0ea7af89
aubravo <alvaroubravo@gmail.com>
2024-05-11 23:34:25 -0600

    project initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Hatch project](https://img.shields.io/badge/%F0%9F%A5%9A-Hatch-4051b5.svg)](https://github.com/pypa/hatch)
---
+++ src/earthgazer/__main__.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+pd.DataFrame({1:1,2:2})
+
+class Test_Class:
+    def __init__(self):
+        self.y = "asdf"
