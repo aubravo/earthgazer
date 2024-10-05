f527b8119d00f191878e63f01996ce1c126d4058
aubravo <alvaroubravo@gmail.com>
2024-10-05 01:25:46 -0600

    feat: Add version and settings info to main application

--- src/earthgazer/__main__.py
+++ src/earthgazer/__main__.py
@@ -1,14 +1,19 @@
-from earthgazer.logger import setup_logger
+import earthgazer.__about__ as about
+from earthgazer.env_management.logger import setup_logger
+from earthgazer.env_management.settings import Settings
 
 # Set up the logger
 logger = setup_logger(__name__)
 
-def main():
-    logger.info("Starting EarthGazer application")
-    logger.debug("This is a debug message")
-    logger.warning("This is a warning message")
-    logger.error("This is an error message")
+settings = Settings()
+
+
+def main() -> None:
+    logger.info(about.__logo__)
+    logger.info(f"earthgazer version {about.__version__}")
+    logger.info(f"Test setting: {settings.test_setting}")
     logger.info("EarthGazer application finished")
+
 
 if __name__ == "__main__":
     main()
