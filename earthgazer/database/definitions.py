1cc0af50eeed4b8381288dc4479acedeba8eca45
aubravo <alvaroubravo@gmail.com>
2025-11-11 19:44:00 +0000

    add locations definition for database

---
+++ earthgazer/database/definitions.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Location(Base):
+    __tablename__ = "locations"
+    __table_args__ = {"schema": "earthgazer"}
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    longitude: Mapped[float]
+    latitude: Mapped[float]
+    from_date: Mapped[str] = mapped_column(DateTime(timezone=False), server_default="now()")
+    to_date: Mapped[str] = mapped_column(DateTime(timezone=False), server_default="now()")
